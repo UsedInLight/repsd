52e7fa4f736e66976821853113119ae6b19e90b3
UsedInLight <76049749+UsedInLight@users.noreply.github.com>
2020-12-17 06:28:11 -0800

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fdb67e4057d16001e799e8d",
+				"5fdb67e4057d16001e799e8f"
+			]
+		},
+		"5fdb67e4057d16001e799e8d": {
+			"id": "5fdb67e4057d16001e799e8d",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fdb67e4057d16001e799e8f": {
+			"id": "5fdb67e4057d16001e799e8f",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
